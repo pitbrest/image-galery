ecced116f446893969dcff314f317640990c73e0
pitbrest <sasha.petrov@tut.by>
2022-02-08 23:21:35 +0300

    some commit

--- js/api.js
+++ js/api.js
@@ -1,12 +1,36 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const url = 'https://api.unsplash.com/search/photos?query=осень&client_id=t6jI0SUjDZOoil0m3Lp3woSwPds8XvNJcun7qmxySN8';
 
 async function getData() {
 	const res = await fetch(url);
-	const data = await res.json();
-	console.log(data);
+	const data = await res.json();	
+	console.log(data);	
+	let array = data.results.map(result => result.urls.regular)
+	for(let item of array) {
+		showData(item)
+	}
+	
 }
-getData();
+getData()
+
+// Генерация элементов
+
+function showData(item) {
+
+	const galleryContainer = document.querySelector('.main');
+	const img = document.createElement('img');
+
+	img.classList.add('gallery-img')
+	img.src = item;
+	img.alt = `image`;
+	galleryContainer.append(img);
+}
 
 
 
 
+
+
+/* 
+const img = `<img class="gallery-img" src="полученный от API адрес изображения" alt="image">`;
+galleryContainer.insertAdjacentHTML('beforeend', img); 
+*/
